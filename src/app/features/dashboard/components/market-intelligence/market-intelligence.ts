8b4202a9bd6bff3ff7e889e7aa791245543c7b9d
Sofiia Popadiuk <popadiuk.sophiya@gmail.com>
2025-09-11 00:46:17 +0300

    add Market Intelligence component

---
+++ src/app/features/dashboard/components/market-intelligence/market-intelligence.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Card } from '../../../../shared/card/card';
+
+@Component({
+  selector: 'app-market-intelligence',
+  imports: [Card],
+  templateUrl: './market-intelligence.html',
+  styleUrl: './market-intelligence.scss'
+})
+export class MarketIntelligence {
+
+}
--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -5,10 +5,11 @@ import { WorkQueue } from './components/work-queue/work-queue';
 import { AsyncPipe } from '@angular/common';
 import { PortfolioGoals } from './components/portfolio-goals/portfolio-goals';
 import { QuickActions } from './components/quick-actions/quick-actions';
+import { MarketIntelligence } from './components/market-intelligence/market-intelligence';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [WorkQueue, AsyncPipe, PortfolioGoals, QuickActions],
+  imports: [WorkQueue, AsyncPipe, PortfolioGoals, QuickActions, MarketIntelligence],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
